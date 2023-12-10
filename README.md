ffdecda7dff2dd0d6429d275d9cbecfb8c69e04a
PSBP-Library <luc.duponcheel@gmail.com>
2023-12-10 15:31:16 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Math Problems
+
+My twin brither, Marc, has often send me interesting math problems.
+
+From time to time I tried to solve them using Scala code as a tool.
+
+## Special Ellipse
+
+Consider the following [picture](png/ellipse.png).
+
+The ratio of the lenghts of the sides of the blue rectangle is `sqrt(2)` (cfr. A4 paper).
+
+The size of the angle between the green lines, through the point on the ellipse
+and a focus of the ellipse, is, as indicated, `π/3` (`60` degrees`).
+
+What is size of the angle between the green slope through the point on the ellipse 
+and the horizontal line (not shown) through the focuses of the ellipse?
+
+The answer, as you can guess from the picture, is `π/4` (`45` degrees`).
+
+*Can you derive it?*
+
+The same property holds, of course, for the point (not shown) on the ellipse
+which is symmetric w.r.t. the middle point (not shown) of the ellipse.
+
+Many other interesting properties follow from this property.
+
+For example: the lenghts of the two blue dashed lines are equal.
+
+I have implemented the derivation of a solution (for the symmetric point).
+
+I also tested the correctness of the derivations and the obtained values.
+
+```scala
+sbt:specialEllipse> test
+[info] Tests:
+[info] - verify deriving angle between focus lines at P
+[info] - deriving equation for y at P
+[info] - verify deriving solution for y at P if α == π/3.0
+[info] - verify deriving solution for x at P if α == π/3.0
+[info] - verify deriving angle of tangent at P if α == π/3.0
+[info] - P is on ellipse
+[info] - angle between focus lines at P is π/3.0
+[info] - angle of tangent at P is π/4.0
+[info] Run completed in 98 milliseconds.
+[info] Total number of tests run: 8
+[info] Suites: completed 1, aborted 0
+[info] Tests: succeeded 8, failed 0, canceled 0, ignored 0, pending 0
+[info] All tests passed.
+[success]
+```
+
---
+++ src/main/scala/specialEllipse/SpecialEllipse.scala
@@ -0,0 +1,97 @@
+package specialEllipse
+
+import scala.math.*
+
+//
+// see README.md for more information
+// about the property of the special ellipse
+//
+// the numbers 1.0, 2.0, 3.0, and 4.0
+// play a special role in the property
+// and its derivation
+//
+// for example in
+// sqrt(1.0 + 3.0) = sqrt(4.0) = 2.0
+// and also in
+//
+// π/(2.0*2.0) resp. π/(2.0*3.0) (π/4.0 resp. π/6)
+//
+
+// the general equation of an ellipse is x*x/a*a + y*y/b*b = 1
+//
+// for the special ellipse we have 1 = a < b = 1/sqrt(2) (cfr A4 paper)
+//
+// the equation of the special ellipse is x*x + 2*y*y = 1 
+
+val π: Double = Pi
+
+type Point = Tuple2[Double, Double]
+
+val derivingAngleBetweenFocusLinesAt: Point => List[Double] =
+
+  val slopeOfLeftFocusLinesAt: Point => Double = (x, y) => y/(x + 1.0/sqrt(2.0))
+
+  val slopeOfRightFocusLinesAt: Point => Double = (x, y) => y/(x - 1.0/sqrt(2.0))
+
+  val angleBetweenFocusLinesAt: Point => Double = (x, y) =>
+    atan(slopeOfLeftFocusLinesAt(x, y)) - atan(slopeOfRightFocusLinesAt(x, y))
+
+  (x, y) =>
+    List(
+      atan(tan(angleBetweenFocusLinesAt(x, y))),
+      // = ...
+      atan(tan(atan(slopeOfLeftFocusLinesAt(x, y)) - atan(slopeOfRightFocusLinesAt(x, y)))),
+      // tan(α - β) = (tan(α) - tan(β))/(1.0 + tan(α) * tan(β))
+      atan(tan(atan(y/(x + 1.0/sqrt(2.0))) - atan(y/(x - 1.0/sqrt(2.0))))),
+      atan((tan(atan(y/(x + 1.0/sqrt(2.0)))) - tan(atan(y/(x - 1.0/sqrt(2.0))))) / 
+      (1.0 + tan(atan(y/(x - 1.0/sqrt(2.0)))) * tan(atan(y/(x + 1.0/sqrt(2.0)))))),
+      atan(((y/(x + 1.0/sqrt(2.0))) - (y/(x - 1.0/sqrt(2.0)))) / (1.0 + (y/(x - 1.0/sqrt(2.0))) * (y/(x + 1.0/sqrt(2.0))))),
+      atan((((- y*x - y/sqrt(2.0) + y*x - y/sqrt(2.0)))/(x*x - 1.0/2.0)) / (1.0 + (y*y/(x*x - 1.0/2.0)))),
+      atan((((- y*x - y/sqrt(2.0) + y*x - y/sqrt(2.0)))/(x*x - 1.0/2.0)) / ((x*x - 1.0/2.0 + y*y)/(x*x - 1.0/2.0))),
+      atan(((- y*x - y/sqrt(2.0) + y*x - y/sqrt(2.0)))/(x*x - 1.0/2.0 + y*y)),
+      atan((- y/sqrt(2.0) - y/sqrt(2.0))/(x*x - 1.0/2.0 + y*y)),
+      atan((- y/sqrt(2.0) - y/sqrt(2.0))/(1.0 - 2.0 * y*y - 1.0/2.0 + y*y)),
+      atan((- y/sqrt(2.0) - y/sqrt(2.0))/(1.0/2.0 - y*y)),
+      // // ... = 
+      atan((-2.0 * y/sqrt(2.0))/(1.0/2.0 - y*y))
+    )
+
+// for tan(π/3.0) = sqrt(3.0)
+//
+// using slope equal to (-2.0 * y/sqrt(2.0))/(1.0/2.0 - y*y)
+// and (for convenience) multplying by sqrt(2.0)
+val derivingEquationForYAt: Point => List[Double] =
+  (x, y) =>
+    List(
+      sqrt(2.0)*(sqrt(3.0)*(1.0/2.0 - y*y) + (2.0 * y/sqrt(2.0))),
+      -sqrt(2.0)*sqrt(3.0)*y*y + 2.0 * y + sqrt(3.0)/sqrt(2.0),
+      -sqrt(2.0*3.0)*y*y + 2.0 * y + sqrt(2.0*3.0)/2.0
+    )
+
+//  (-b + sqrt(b*b - a*c))/a
+val derivingSolutionForY: List[Double] = List(
+  (-1.0 + sqrt(1.0 + 3.0)) / -sqrt(2.0*3.0),
+  (1.0 - sqrt(1.0 + 3.0)) / sqrt(2.0*3.0),
+  (1.0 - sqrt(4.0))/sqrt(2.0*3.0),
+  (1.0 - 2.0)/sqrt(2.0*3.0),
+  -1.0/sqrt(2.0*3.0)
+)
+
+// x = sqrt(1.0 - 2.0*y*y)
+val derivingSolutionForX: List[Double] = List(
+  sqrt(1.0 - 2.0 * (-1.0/sqrt(2.0*3.0)) * (-1.0/sqrt(2.0*3.0))),
+  sqrt(1.0 - 2.0 * (1.0/sqrt(2.0*3.0)) * (1.0/sqrt(2.0*3.0))),
+  sqrt(1.0 - 2.0/(2.0*3.0)),
+  sqrt(1.0 - 1.0/3.0),
+  sqrt(2.0/3.0)
+)
+
+// ∃γ P = (cos(γ), sin(γ)/sqrt(2.0))
+// the slope of the tangent at P is -sin(γ) / (cos(γ)/sqrt(2.0))
+// in other words, without using γ
+// the slope of the tangent at P is -(y*sqrt(2.0)) / (x/sqrt(2.0))
+val derivingAngleOfTangent: List[Double] = List(
+  atan((1.0/sqrt(2.0*3.0)*sqrt(2.0)) / ((sqrt(2.0/3.0))/sqrt(2.0))),
+  atan((1.0/(sqrt(3.0)))/(1.0/sqrt(3.0))),
+  atan(1.0)
+)
