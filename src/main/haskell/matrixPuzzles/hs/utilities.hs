3893f1c2d85020bacd2161a9fcbebcfd1792a690
LucDuponcheelAtGitHub <luc.duponcheel@gmail.com>
2024-09-25 14:56:04 +0200

    removes instances.hs

--- src/main/haskell/matrixPuzzles/hs/utilities.hs
+++ src/main/haskell/matrixPuzzles/hs/utilities.hs
@@ -1,7 +1,25 @@
 module Utilities where
 
 import Data.List (nub, (\\))
-import Instances
+
+-- module Instances where
+
+instance Num (Maybe Integer) where
+  negate (Just x) = Just (-x)
+  negate _ = Nothing
+  (+) (Just x) (Just y) = Just (x + y)
+  (+) _ _ = Nothing
+  (*) (Just x) (Just y) = Just (x * y)
+  (*) _ _ = Nothing
+  abs (Just x) = Just (abs x)
+  abs _ = Nothing
+  signum (Just x)
+    | x >= 0 = Just 1
+    | otherwise = Just (-1)
+  signum _ = Nothing
+  fromInteger = Just
+
+-- import Instances
 
 cp :: [[z]] -> [[z]]
 cp [] = [[]]
--- src/main/haskell/matrixPuzzles/sh/makeAllSudokuSolutionsMain.sh
+++ src/main/haskell/matrixPuzzles/sh/makeAllSudokuSolutionsMain.sh
@@ -5,7 +5,7 @@ cd ../hs
 mv ../bin/*.hi .
 mv ../bin/*.o .
 
-ghc -O2 allSudokuSolutionsMain.hs allSudokuSolutionsFor.hs sudokuSpecifics.hs matrixPuzzles.hs utilities.hs instances.hs
+ghc -O2 allSudokuSolutionsMain.hs allSudokuSolutionsFor.hs sudokuSpecifics.hs matrixPuzzles.hs utilities.hs
 
 mv *.o ../bin
 mv *.hi ../bin
